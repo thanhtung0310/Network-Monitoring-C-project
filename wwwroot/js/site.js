d7330d152a3cb74f1ebe6a11164a125661ae9dfc
thanhtung0310 <thanhtung031099@gmail.com>
2022-03-24 21:21:50 +0700

    console page with tab and content

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+function openContent(evt, pageName) {
+    // khai bao bien
+    var i, maincontent, tablinks;
+
+    // lay tat thuoc tinh thuoc class="maincontent" va hide
+    maincontent = document.getElementsByClassName("maincontent");
+    for (i = 0; i < maincontent.length; i++) {
+        maincontent[i].style.display = "none";
+    }
+
+    //
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    //
+    document.getElementById(pageName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
